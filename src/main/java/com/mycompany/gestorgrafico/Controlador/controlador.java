0b1e6be10145b4c332007cc51ff3492690a7517b
Jost1nVx7 <jostinjd2002@gmail.com>
2025-02-06 03:32:13 -0500

    nuevo

--- src/main/java/com/mycompany/gestorgrafico/Controlador/controlador.java
+++ src/main/java/com/mycompany/gestorgrafico/Controlador/controlador.java
@@ -1,4 +1,3 @@
-
 package com.mycompany.gestorgrafico.Controlador;
 
 import com.mycompany.gestorgrafico.Modelo.gestorTareas;
@@ -6,33 +5,38 @@ import com.mycompany.gestorgrafico.Modelo.tarea;
 import com.mycompany.gestorgrafico.Vista.Vista;
 import java.util.List;
 
-
 public class controlador {
+
     private Vista vista;
-    private gestorTareas gestorTareas;;
+    private gestorTareas gestorTareas;
+
+    ;
 
     public controlador(Vista vista) {
         this.vista = vista;
         this.gestorTareas = new gestorTareas();
     }
-    
+
     /*public void porcesoGestorTareas(){
         /*si los datos estan correctos pasa al modelo
         caso contrario los devuelve a la vista
-        */
-    public void agregarTarea(int id, String titulo, String descripcion, boolean tareaCompletada) {
-        gestorTareas.agregarTarea(id, titulo, descripcion, tareaCompletada);
-    }
+     */
+    
+        public void agregarTarea(int id, String titulo, String descripcion, boolean tareaCompletada) {
+            gestorTareas.agregarTarea(id, titulo, descripcion, tareaCompletada);
+        }
 
-    public List<tarea> listarTareaPendiente() {
-        return gestorTareas.listarTareaPendiente();
-    }
+        public List<tarea> listarTareaPendiente() {
+            return gestorTareas.listarTareaPendiente();
+        }
 
-    public List<tarea> listarTareaCompletada() {
-        return gestorTareas.listarTareasCompletadas();
-    }
+        public List<tarea> listarTareaCompletada() {
+            return gestorTareas.listarTareaCompleta();
+        }
 
-    public void marcarComoCompleta(int id) {
-        gestorTareas.marcarComoCompleta(id);
+        public void marcarComoCompleta(int id) {
+            gestorTareas.marcarComoCompleta(id);
+        }
+
     }
 }
